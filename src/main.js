564b9bf24a86fc2302e0582a654ff288dd568531
Kenny Urena <urenakenny@outlook.com>
2019-06-13 20:43:35 -0400

    vue-resource introduction

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
+import VueResource from "vue-resource";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import BootstrapVue from "bootstrap-vue";
@@ -8,8 +9,8 @@ import PortalVue from "portal-vue";
 
 Vue.use(PortalVue);
 Vue.use(BootstrapVue);
+Vue.use(VueResource);
 Vue.config.productionTip = false;
-
 
 new Vue({
   render: h => h(App)
